68d1c5e499a0220dc499ab1243af83a8ac4628f5
GromkovAlexander <gromkovalex@mail.ru>
2019-12-26 14:56:35 +0300

    Scripted auto-commit on change (2019-12-26 14:56:35) by gitwatch.sh

--- src/main/java/lab5/App.java
+++ src/main/java/lab5/App.java
@@ -1,25 +1,57 @@
 package lab5;
 
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
+import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+
+
+import static akka.actor.TypedActor.context;
+
 public class App {
     public static void main(String[] args) throws IOException {
         System.out.println("start!");
         ActorSystem system = ActorSystem.create("routes");
+
         final Http http = Http.get(system);
-        final ActorMaterializer materializer =
-                ActorMaterializer.create(system);
-        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = <вызов
-        метода которому передаем Http, ActorSystem и ActorMaterializer>;
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = 
+        //<вызов метода которому передаем Http, ActorSystem и ActorMaterializer>;
+
+
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
                 routeFlow,
                 ConnectHttp.toHost("localhost", 8080),
                 materializer
         );
-        System.out.println("Server online at http://localhost:8080/\nPress
-                RETURN to stop...");
+
+        System.out.println("Server online at http://localhost:8080/\nPress RETURN to stop...");
         System.in.read();
+
         binding
                 .thenCompose(ServerBinding::unbind)
-                .thenAccept(unbound -> system.terminate()); // and shutdown
-        when done
+                .thenAccept(unbound -> system.terminate());
+        // and shutdown when done
     }
 }
